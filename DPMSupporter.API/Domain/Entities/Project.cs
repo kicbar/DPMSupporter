38c88f2dbdfbe5a6916cf2c870c07f169bfc011b
kicbar <bartlomiejkicior@gmail.com>
2022-08-26 22:58:56 +0200

    add core entities

---
+++ DPMSupporter.API/Domain/Entities/Project.cs
@@ -0,0 +1,9 @@
+﻿namespace DPMSupporter.API.Domain.Entities
+{
+    public class Project
+    {
+        public string ProjectName { get; set; }
+        public string ProjectShortName { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ DPMSupporter.API/Domain/Entities/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DPMSupporter.API.Domain.Entities
+{
+    public class Task
+    {
+        public Guid ID { get; set; }
+        public string TaskName { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        public bool IsImplemented { get; set; } = false;
+        public DateTime ImplementedDate { get; set; }
+    }
+}
