19d435b5740edd0f29318edac6f1b494f85a958e
kicbar <bartlomiejkicior@gmail.com>
2022-08-27 14:37:04 +0200

    Add different dtos for read and write

---
+++ DPMSupporter.API/Application/DTOs/ProjectWriteDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DPMSupporter.API.Application.DTOs
+{
+    public class ProjectWriteDto
+    {
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+    }
+}
