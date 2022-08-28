33aa6193b76c94a1f60e5ea0e09505981c44cbf6
kicbar <bk.7@o2.pl>
2022-08-28 14:49:27 +0200

    implement task repository

--- DPMSupporter.API/Infrastructure/Repositories/IRepositories/ITaskRepository.cs
+++ DPMSupporter.API/Infrastructure/Repositories/IRepositories/ITaskRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace DPMSupporter.API.Infrastructure.Repositories.IRepositories
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DPMSupporter.API.Infrastructure.Repositories.IRepositories
 {
     public interface ITaskRepository
     {
+        Task<Domain.Entities.Task> AddTask(Domain.Entities.Task task);
+        Task<bool> DeleteTask(Guid taskId);
+        Task<List<Domain.Entities.Task>> GetAllTasks();
+        Task<Domain.Entities.Task> GetTask(Guid taskId);
+        Task<Domain.Entities.Task> UpdateTask(Domain.Entities.Task task);
     }
 }
--- DPMSupporter.API/Infrastructure/Repositories/TaskRepository.cs
+++ DPMSupporter.API/Infrastructure/Repositories/TaskRepository.cs
@@ -1,5 +1,10 @@
 ﻿using DPMSupporter.API.Infrastructure.Data;
 using DPMSupporter.API.Infrastructure.Repositories.IRepositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DPMSupporter.API.Infrastructure.Repositories
 {
@@ -10,6 +15,54 @@ namespace DPMSupporter.API.Infrastructure.Repositories
         public TaskRepository(DPMSupporterDbContext DPMSupporterDbContext)
         {
             _DPMSupporterDbContext = DPMSupporterDbContext;
+        }
+
+        public async Task<Domain.Entities.Task> AddTask(Domain.Entities.Task task)
+        {
+            var createdTask = await _DPMSupporterDbContext.Tasks.AddAsync(task);
+            await _DPMSupporterDbContext.SaveChangesAsync();
+            return createdTask.Entity;
+        }
+
+        public async Task<List<Domain.Entities.Task>> GetAllTasks()
+        {
+            return await _DPMSupporterDbContext.Tasks.ToListAsync();
+        }
+
+        public async Task<Domain.Entities.Task> GetTask(Guid taskId)
+        {
+            return await _DPMSupporterDbContext.Tasks.Where(t => t.Id == taskId).FirstOrDefaultAsync();
+        }
+
+        public async Task<Domain.Entities.Task> UpdateTask(Domain.Entities.Task task)
+        {
+            var taskExist = await _DPMSupporterDbContext.Tasks.FirstOrDefaultAsync(t => t.Id == task.Id);
+            if (taskExist != null)
+            {
+                var updatedTask = _DPMSupporterDbContext.Tasks.Update(task);
+                await _DPMSupporterDbContext.SaveChangesAsync();
+                return updatedTask.Entity;
+            }
+            throw new Exception("Updated not executed.");
+        }
+
+        public async Task<bool> DeleteTask(Guid taskId)
+        {
+            try
+            {
+                var taskExist = await _DPMSupporterDbContext.Tasks.FirstOrDefaultAsync(t => t.Id == taskId);
+                if (taskExist != null)
+                {
+                    _DPMSupporterDbContext.Tasks.Remove(taskExist);
+                    await _DPMSupporterDbContext.SaveChangesAsync();
+                }
+                return true;
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine($"Error occured: {exc.Message}");
+                return false;
+            }
         }
     }
 }
