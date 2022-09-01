f1b3bbb7a42985b2df483cccc87029aa196d23d7
kicbar <bk.7@o2.pl>
2022-09-01 15:43:45 +0200

    remove projectId from taskwritedto

--- DPMSupporter.API/Application/DTOs/TaskWriteDto.cs
+++ DPMSupporter.API/Application/DTOs/TaskWriteDto.cs
@@ -1,12 +1,9 @@
-﻿using System;
-
-namespace DPMSupporter.API.Application.DTOs
+﻿namespace DPMSupporter.API.Application.DTOs
 {
     public class TaskWriteDto
     {
         public string TaskName { get; set; }
         public string Description { get; set; }
         public bool IsImplemented { get; set; }
-        public Guid ProjectId { get; set; }
     }
 }
